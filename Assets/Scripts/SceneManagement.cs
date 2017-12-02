393aec49cfc88d12a00bfaadb9e0259be2d34bd9
Henrik Söderberg <henkkasoderberg@hotmail.com>
2017-12-02 15:14:53 +0200

    main menu with scene manager

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement; 
+
+public class SceneManagement : MonoBehaviour {
+
+	//Change scene based on scene name
+	public void changeScene(string scene){
+		SceneManager.LoadScene (scene);
+	}
+}
