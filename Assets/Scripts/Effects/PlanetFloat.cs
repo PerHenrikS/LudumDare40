c7f2dcf76ae7fcb3b62bbc47944ac5d145cc668a
Henrik Söderberg <henkkasoderberg@hotmail.com>
2017-12-03 15:13:42 +0200

    Levels 5-6

---
+++ Assets/Scripts/Effects/PlanetFloat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetFloat : MonoBehaviour {
+	//Script will be attached to the planet which is going to float 
+
+	private float originalPos; 
+	public float floatSpeed = 2f; 
+	public float floatStrength = 2f; 
+
+	void Start () {
+		originalPos = transform.position.y; 
+	}
+
+	void Update(){
+		transform.localPosition = new Vector3(transform.position.x,
+			originalPos + ((float)Mathf.Sin(Time.time * floatSpeed) * floatStrength),
+			transform.position.z);
+	}
+}
