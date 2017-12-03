05c74809a2d67a8821a0c5ca8363cab329f985e9
Henrik Söderberg <henkkasoderberg@hotmail.com>
2017-12-03 11:10:00 +0200

    Level 1 and controller prefab

--- Assets/Scripts/Effects/MoveCam.cs
+++ Assets/Scripts/Effects/MoveCam.cs
@@ -7,10 +7,26 @@ public class MoveCam : MonoBehaviour {
 	//If i want to restrict cam movement all together on some scene
 	public bool moveable = true; 
 
-	public float moveSpeed = .01f; 
+	public float moveSpeed = .04f; 
+	public float zoomSpeed = 1f;
+
+	public GameObject gameController; 
+	private Selection selector; 
+
+	void Start(){
+		selector = gameController.GetComponent<Selection> ();
+	}
 
 	// Update is called once per frame
 	void Update () {
+		if(selector.getSelectedId() == ""){
+			if(Input.GetAxis("Mouse ScrollWheel") < 0f){
+				Camera.main.orthographicSize += zoomSpeed; 
+			}
+			if(Input.GetAxis("Mouse ScrollWheel") > 0f){
+				Camera.main.orthographicSize -= zoomSpeed; 
+			}
+		}
 		if(moveable){
 			if(Input.GetKey("w")){
 				transform.Translate (0f, moveSpeed, 0f);
@@ -24,6 +40,6 @@ public class MoveCam : MonoBehaviour {
 			if(Input.GetKey("s")){
 				transform.Translate (0f, -moveSpeed, 0f);
 			}
-		}	
+		}
 	}
 }
